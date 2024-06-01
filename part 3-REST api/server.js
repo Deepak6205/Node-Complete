015d52326c534fb964ec1313072d8ca20bf8ba9c
deepak kumar <deepak7292832956@gmail.com>
2024-06-01 15:15:10 +0530

    this session was for creating the server for our app.

---
+++ part 3-REST api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Welcome Baby Lets start this shit...');
+});
+app.get('/chicken',(req,res) => {
+    res.send('sure baby we will fuck it..lets do this shit');
+});
+app.get('/idli', (req,res) => {
+    var customized_idli = {
+        name: 'rava idli',
+        size: '10cm dia wala',
+        is_sambhar: true,
+        is_coconut_chatni: false
+    }
+    res.send(customized_idli);
+})
+
+app.listen(3000, () => {
+    console.log('listening to the server');
+});
